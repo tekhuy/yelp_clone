00b774605adf0b403f98bce09d5b0487cb448742
ciawalsh <cia.walsh@gmail.com>
2015-02-17 14:16:25 +0000

    Form Working! All Tests Passing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,4 @@ Rails.application.routes.draw do
 
   resources :restaurants          # same as --> get 'restaurants' => 'restaurants#index'
   
-  resources :new_restaurant      #get 'restaurants/new' => 'restaurants#new'
-
 end
