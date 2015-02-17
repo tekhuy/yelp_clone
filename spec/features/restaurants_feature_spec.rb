3aa8afce8f886aeab99496cf5044e67a02df0f4c
tekhuy <huyle20@hotmail.com>
2015-02-17 17:05:07 +0000

    delete restaurant test passed

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -13,9 +13,7 @@ feature 'restaurants' do
   end
 
   context 'restaurants have been added' do
-    before do
-      Restaurant.create(name: 'KFC')
-    end
+    before {Restaurant.create name: 'KFC'}
 
     scenario 'display restaurants' do
       visit '/restaurants'
@@ -57,6 +55,17 @@ feature 'restaurants' do
       click_button 'Update Restaurant'
       expect(page).to have_content 'Kentucky Fried Chicken'
       expect(current_path).to eq '/restaurants'
+    end
+  end
+
+  context 'deleting restaurants' do
+    before {Restaurant.create name: 'KFC'}
+
+    scenario 'removes a restaurant when a user clicks a delete link' do
+      visit '/restaurants'
+      click_link 'Delete KFC'
+      expect(page).not_to have_content 'KFC'
+      expect(page).to have_content 'Restaurant deleted successfully'
     end
   end
 
