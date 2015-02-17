7f02c479db3824b0fad056ae4a07576db3b8bf37
tekhuy <huyle20@hotmail.com>
2015-02-17 13:04:38 +0000

    path to new restaurant created, test fail

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
   resources :restaurants          # same as --> get 'restaurants' => 'restaurants#index'
+  
+  resources :new_restaurant      #get 'restaurants/new' => 'restaurants#new'
 
 end
