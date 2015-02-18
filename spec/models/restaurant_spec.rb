edb289013ccf5803e1e97549a6250600f6ed186f
ciawalsh <cia.walsh@gmail.com>
2015-02-18 11:03:29 +0000

    Version 1 complete

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Restaurant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Restaurant, :type => :model do
+  
+  it 'is not valid with a name of less than three characters' do
+    restaurant = Restaurant.new(name: "kf")
+    expect(restaurant).to have(1).error_on(:name)
+    expect(restaurant).not_to be_valid
+  end
+
+  it "is not valid unless it has a unique name" do
+    Restaurant.create(name: "Moe's Tavern")
+    restaurant = Restaurant.new(name: "Moe's Tavern")
+    expect(restaurant).to have(1).error_on(:name)
+  end
+
 end
