7805275d5df9f6442c7197cde827ea0926200faa
ciawalsh <cia.walsh@gmail.com>
2015-02-18 12:14:47 +0000

    Added Sign in & Log in

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :restaurants do        # same as --> get 'restaurants' => 'restaurants#index'
     resources :reviews
+    root to: "restaurants#index"
   end
 
 end
