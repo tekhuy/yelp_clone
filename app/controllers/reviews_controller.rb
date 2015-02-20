5922dce674a69af935af72a48afd3b5560962bb5
tekhuy <huyle20@hotmail.com>
2015-02-20 23:49:17 +0000

    added tests... Not completed V2

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,25 +1,37 @@
 class ReviewsController < ApplicationController
+  
+  include ReviewsHelper
 
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = Review.new
+    if current_user.has_reviewed?(@restaurant)
+      flash[:notice] = 'You can only leave one review for a restaurant'
+      redirect_to restaurants_path
+    else
+      @review = Review.new
+    end
   end
 
   def create 
     @restaurant = Restaurant.find(params[:restaurant_id])
-    if current_user.has_reviewed?(@restaurant)
-      error message
-    else
-      review = @restaurant.reviews.create(review_params)
-      review.user = current_user
-      review.save
+    @review = @restaurant.reviews.new(review_params)
+    @review.user = current_user
+    if @review.save
       redirect_to restaurants_path
+    else
+      render 'new'
     end
-
   end
 
   def review_params
     params.require(:review).permit(:thoughts, :rating)
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    flash[:notice] = 'Review deleted successfully'
+    redirect_to '/restaurants'
   end
 
 end
--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -31,4 +31,55 @@ feature 'reviewing' do
     expect(page).not_to have_link('Edit KFC')
   end
 
+  scenario 'that reviews get deleted if the restaurant gets deleted' do
+    sign_up('test')
+    add_restaurant('KFC')
+    visit '/restaurants'
+    click_link 'Review KFC'
+    fill_in "Thoughts", with: "so, so"
+    select '3', from: 'Rating'
+    click_button 'Leave Review'
+    click_link 'Delete KFC'
+    expect(Review.all.any?).to be false
+  end
+
+  scenario 'users can only leave one review per restaurant' do
+    sign_up('test')
+    add_restaurant('KFC')
+    leave_review('KFC', "so, so", "3")
+    click_link 'Review KFC'
+    expect(current_path).to eq '/restaurants'
+    expect(page).to have_content('You can only leave one review per restaurant')
+  end
+
+  scenario 'users can see link to delete their own reviews' do
+    sign_up('test')
+    add_restaurant('KFC')
+    leave_review('KFC', "so, so", "3")
+    expect(page).to have_link('Delete review')
+  end
+
+  scenario 'users can not delete others reviews' do
+    sign_up('test')
+    add_restaurant('KFC')
+    leave_review('KFC', "so, so", "3")
+    click_link('Sign out')
+    sign_up('bob')
+    expect(page).not_to have_link('Delete review')
+  end
+
+  scenario 'users can delete their own reviews' do
+    sign_up('test')
+    add_restaurant('KFC')
+    leave_review('KFC', "so, so", "3")
+    click_link('Delete review')
+    expect(page).not_to have_content("so, so")
+    expect(page).to have_content('Review deleted successfully')
+  end
+
 end
+
+
+
+
+
